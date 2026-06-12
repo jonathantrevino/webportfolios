adf502ee7323d5f143648d7ffd35dd6c046866a8
Jonathan Trevino <yeahimjt+github@gmail.com>
2026-06-12 11:50:13 -0500

    adding necessary outputs for cognito

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,15 @@
+# cognito
+output "cognito_user_pool_id" {
+  description = ""
+  value = aws_cognito_user_pool.pool.id
+}
 
+output "cognito_user_pool_arn" {
+  description = ""
+  value = aws_cognito_user_pool.pool.arn
+}
+
+output "cognito_user_pool_client_id" {
+  description = ""
+  value = aws_cognito_user_pool_client_id.client.client_id
+}
